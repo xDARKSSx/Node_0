e1c9d67d4df45d2e3a87914530dd054b69070a89
xDARKSSx <imail4dark@gmail.com>
2026-07-01 02:41:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,27 @@ function go() {
     r.innerText = "NODE_0: pattern unclear.";
   }
 }
+const targetDate = new Date("2026-07-02T00:00:00").getTime();
+
+const x = setInterval(function() {
+
+  const now = new Date().getTime();
+  const distance = targetDate - now;
+
+  if (distance <= 0) {
+    clearInterval(x);
+
+    document.getElementById("timer").innerText = "";
+    document.getElementById("hidden").style.display = "block";
+
+    return;
+  }
+
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("timer").innerText =
+    `NODE_0 ACTIVE IN: ${hours}h ${minutes}m ${seconds}s`;
+
+}, 1000);
